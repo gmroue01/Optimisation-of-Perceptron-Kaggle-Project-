e335eaa23bf902669a454fafab7d9ab169040ae0
gmroue01 <95682994+gmroue01@users.noreply.github.com>
2026-02-14 22:02:57 +0100

    try to dowload data

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 
-
+import Perceptron_GD
 
 if __name__ == "__main__":
-    print("Hello World !!!")
+    
+    data = scipy.io.loadmat("data_doc.mat",squeeze_me=True,struct_as_record=False)
+    print(data.keys())
 
+    # Input
+    X = data['Xts'].tocsr()
+    # Label
+    y_ts = data['yts']
+    #Validation
+    X_vr = data['Xvr'].tocsr()
+
+    # Definition of index
+    n = X.shape[0]
+    d = X.shape[1]
 
 
