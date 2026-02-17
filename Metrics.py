74f4a39dd3484e27019d7886a696a02b7bb81ee3
gmroue01 <95682994+gmroue01@users.noreply.github.com>
2026-02-17 14:01:59 +0100

    Ajoute de la classe Metrics

---
+++ Metrics.py
@@ -0,0 +1,26 @@
+
+
+class Metrics:
+    
+    def __init__(self,y_pred,y_true):
+        self.y_pred = y_pred
+        self.y_true = y_true
+        self.accuracy = None
+        self.loss = None
+        self.recall = None
+        self.precision = None
+        
+    def accuracy_score(self):
+        pass
+    def loss_score(self):
+        pass
+    
+    def recall_score(self):
+        pass
+    
+    def precision_score(self):
+        pass
+    
+
+        
+        
