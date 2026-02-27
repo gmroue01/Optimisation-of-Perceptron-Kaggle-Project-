cbb2fcb3cce9346f3812465509b5516eeb14d5fa
unknown <gmroue01@gmail.com>
2026-02-27 14:45:28 +0100

    Iteration 4

--- Data.py
+++ Data.py
@@ -1,6 +1,29 @@
 from sklearn.preprocessing import MaxAbsScaler
-
+import scipy.io
+from sklearn.model_selection import train_test_split
 class DataPreprocessing :
 
-    def __init__(self):
-        pass
+    def __init__(self, filepath,seed):
+        self.file = filepath
+        self.data = scipy.io.loadmat("./data/data_doc.mat",
+                            squeeze_me=True, struct_as_record=False)
+        self.X = self.data['Xts'].tocsr()
+        self.y_ts = self.data['yts']
+        self.X_vr = self.data['Xvr'].tocsr()
+        self.seed = seed
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
+
+
+
+    def fit(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+        self.X, self.y_ts, test_size=0.2, random_state=self.seed)
+
+        return self.X_train,self.y_train,self.X_test,self.y_test
+    
+
+
+    
--- main.py
+++ main.py
@@ -3,46 +3,40 @@ import scipy.io
 import numpy as np
 from scipy.sparse import vstack
 from Perceptron import Perceptron_GD
+from Data import DataPreprocessing
 import matplotlib.pyplot as plt
 from OptimalStep import puissance
 from Metrics import Metrics
 from sklearn.model_selection import train_test_split
 
+
+
+
+
 if __name__ == "__main__":
 
-    data = scipy.io.loadmat("./data/data_doc.mat",
-                            squeeze_me=True, struct_as_record=False)
+    d = DataPreprocessing("./data/data_doc.mat",seed=42)
+                            
 
-    X = data['Xts'].tocsr()
+    X_train, y_train, X_test,y_test = d.fit()
 
-    y_ts = data['yts']
 
-    X_vr = data['Xvr'].tocsr()
-
-    # labels = [i for i in range(20)]
-
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     X, y_ts, test_size=0.2, random_state=42, stratify=labels)
-
-    labels = 20
-
-    n = X.shape[1]
     # Calcul de Hessienne
-    H = (1/n)*X@X.T
+   
 
     # Calcul de la valeur propre maximum de H
     # print("Calcul de la valeur propre de module maximum")
     # L = puissance(H)
     # print(f"La valeur propre de module maximum vaut {L}")
 
-    model = Perceptron_GD(
-        input_size=X.shape[0], output_size=labels, learning_rate=1, n_iters=100)
-    model.fit(X, y_ts)
+    # model = Perceptron_GD(
+    #     input_size=X.shape[0], output_size=labels, learning_rate=1, n_iters=100)
+    # model.fit(X, y_ts)
 
-    # Plot the loss history
-    plt.plot(model.loss_history)
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.title("Loss History")
-    plt.savefig("./results/loss_history.png")
-    print("Loss history saved to loss_history.png")
+    # # Plot the loss history
+    # plt.plot(model.loss_history)
+    # plt.xlabel("Epoch")
+    # plt.ylabel("Loss")
+    # plt.title("Loss History")
+    # plt.savefig("./results/loss_history.png")
+    # print("Loss history saved to loss_history.png")
